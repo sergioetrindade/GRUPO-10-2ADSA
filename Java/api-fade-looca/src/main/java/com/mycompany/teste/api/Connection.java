be7f7f528124593dffd179723dd3eda7de71d08b
sergioetrindade <sergio.trindade@bandtec.com.br>
2021-04-14 19:27:51 -0300

    Banco de dados local e api com conecao sql

---
+++ Java/api-fade-looca/src/main/java/com/mycompany/teste/api/Connection.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.teste.api;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+/**
+ *
+ * @author Sérgio
+ */
+public class Connection {
+    
+    private BasicDataSource dataSource;
+    
+    public Connection() {
+        dataSource = new BasicDataSource();
+        dataSource​.setDriverClassName("\"com.mysql.cj.jdbc.Driver");
+        dataSource​.setUrl("jdbc:mysql://localhost:3306/Localhost Fade");  
+        dataSource​.setUsername("fade");
+        dataSource​.setPassword("urubu1@@");
+        
+        
+        
+        
+    }
+}
